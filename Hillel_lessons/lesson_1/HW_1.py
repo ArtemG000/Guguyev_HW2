6a1de4a329b8695f6c0865dfcb3b335229b6c5c5
ArtemG000 <artemguguev13@gmail.com>
2025-03-13 22:46:35 +0100

    Delete Hillel_lessons/lesson_1 directory

--- Hillel_lessons/lesson_1/HW_1.py
+++
@@ -1,5 +0,0 @@
-print('Hello, World!') #Artem Guguyev
-print('Hello, World!') #Artem Guguyev
-print('Hello, World!') #Artem Guguyev
-print('Hello, World!') #Artem Guguyev
-print('Hello, World!') #Artem Guguyev
