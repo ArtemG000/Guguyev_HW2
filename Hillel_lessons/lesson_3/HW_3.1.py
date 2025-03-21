bf414a690ff520c27755225df8622a8fec670b34
ArtemG000 <artemguguev13@gmail.com>
2025-03-22 00:53:28 +0100

    HW_3_1 done

---
+++ Hillel_lessons/lesson_3/HW_3.1.py
@@ -0,0 +1,26 @@
+#Програма має виконувати прості математичні дії (+, -, *, /). Користувачеві пропонується почерзі ввести числа та дію над цими числами, а програма, виходячи з дії, обчислює та друкує результат.
+#Зробити перевірку на те, що при діленні дільник не дорівнює 0!
+#Artem Guguyev
+
+print('Вітаємо в простому калькуляторі!')
+user_input_number_1 = float(input('Введіть перше число - '))
+user_input_number_2 = float(input('Введіть друге число - '))
+user_input_act = input('Введіть дію ( + , - . * . / ) - ')
+#if user_input_act != '+' and user_input_act != '-' and user_input_act != '*' and user_input_act != '/':
+    #print('Дія не розпізнана')
+if user_input_act == '+':
+    result_ = user_input_number_1 + user_input_number_2
+    print(result_)
+elif user_input_act == '-':
+    result_ = user_input_number_1 - user_input_number_2
+    print(result_)
+elif user_input_act == '*':
+    result_ = user_input_number_1 * user_input_number_2
+    print(result_)
+elif user_input_act == '/':
+    result_ = user_input_number_1 / user_input_number_2
+    print(result_)
+else:
+    print('Дія не розпізнана')
+print('Кінець')
+#Не подобається мені, що при введенні знаку дії не дозволяються пробіли.
