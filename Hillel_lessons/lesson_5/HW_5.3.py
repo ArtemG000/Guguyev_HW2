3123c32baa45ea1b8d5c37f32a51431e88ddfdd8
ArtemG000 <artemguguev13@gmail.com>
2025-03-31 00:13:19 +0200

    HW_5_3 redone

--- Hillel_lessons/lesson_5/HW_5.3.py
+++ Hillel_lessons/lesson_5/HW_5.3.py
@@ -11,13 +11,21 @@
 import string
 
 list_pun = list(string.punctuation)
-usr_input = input('Введіть текст хештегу - ').strip().lower()
-usr_input = "".join(el for el in usr_input if el not in list_pun).split()
-for i, word in enumerate(usr_input):
-    usr_input[i] = word.title()
+usr_input = input('Введіть текст хештегу - ')
+for n in list_pun:
+    usr_input = usr_input.replace(n, ' ')
+usr_input = usr_input.split()
+for i in (range(len(usr_input))):
+    usr_input[i] = usr_input[i].title()
 usr_input = ''.join(usr_input)
-
 print(f"#{usr_input[:140]}")
+
+#usr_input = "".join(el for el in usr_input if el not in list_pun).split()
+# for i, word in enumerate(usr_input):
+#     usr_input[i] = word.title()
+# usr_input = ''.join(usr_input)
+#
+# print(f"#{usr_input[:140]}")
 
 #end_ = '#{}'
 #print(end_.format(usr_input[:140]))
