8bbd8405635404194edb6d833c0fc678b801bd03
ArtemG000 <artemguguev13@gmail.com>
2025-03-31 23:15:34 +0200

    HW_6_3 done

---
+++ Hillel_lessons/lesson_6/HW_6.3.py
@@ -0,0 +1,11 @@
+# Ваше завдання — написати програму, яка перемножує всі цифри, введені користувачем цілого числа, поки воно не стане менше або дорівнювати 9.
+# Користувач вводить число з клавіатури.
+# Artem Guguyev
+
+
+usr_input = list(input('Enter a number - '))
+usr_input = eval(' * '.join(usr_input))
+while usr_input > 9:
+    usr_input = list(str(usr_input))
+    usr_input = eval(' * '.join(usr_input))
+print(usr_input)
