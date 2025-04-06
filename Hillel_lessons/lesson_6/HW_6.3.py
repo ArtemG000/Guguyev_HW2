99a4109081c83b7f8d363f01380bb63405d70fb8
ArtemG000 <artemguguev13@gmail.com>
2025-04-06 12:43:31 +0200

    HW_7_1 done

--- Hillel_lessons/lesson_6/HW_6.3.py
+++ Hillel_lessons/lesson_6/HW_6.3.py
@@ -3,8 +3,13 @@
 # Artem Guguyev
 
 
-usr_input = list(input('Enter a number - '))
-usr_input = eval(' * '.join(usr_input))
+# usr_input = list(input('Enter a number - '))
+# usr_input = eval(' * '.join(usr_input))
+# while usr_input > 9:
+#     usr_input = list(str(usr_input))
+#     usr_input = eval(' * '.join(usr_input))
+# print(usr_input)
+usr_input = int(input('Enter a number - '))
 while usr_input > 9:
     usr_input = list(str(usr_input))
     usr_input = eval(' * '.join(usr_input))
