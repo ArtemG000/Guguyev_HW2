853cc820363df6c26eba5b9fb6ab3eadb9e97aef
ArtemG000 <artemguguev13@gmail.com>
2025-04-06 13:44:17 +0200

    HW_7_2 done

--- Hillel_lessons/lesson_7/HW_7.2.py
+++ Hillel_lessons/lesson_7/HW_7.2.py
@@ -6,11 +6,14 @@
 
 
 def correct_sentence(text):
-    text = text.rstrip('.').capitalize() + '.'
+    text = list(text.rstrip('.'))
+    text[0] = text[0].upper()
+    text = ''.join(text) + '.'
     return text
-#assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-#assert correct_sentence("hello") == "Hello.", 'Test2'
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
-#assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-#assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
 print('ОК')
