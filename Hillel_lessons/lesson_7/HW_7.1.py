875aaaaed63d5ca1fefa0bdde966d4c4278a9e7d
ArtemG000 <artemguguev13@gmail.com>
2025-04-06 12:45:07 +0200

    HW_7_1 done

--- Hillel_lessons/lesson_7/HW_7.1.py
+++ Hillel_lessons/lesson_7/HW_7.1.py
@@ -6,8 +6,7 @@
 def say_hi(name, age):
   name = input('Enter your name -')
   age = int(input('Enter your age -'))
-  if name and age:
-      return f'Hi. My name is {name} and I\'m {str(age)} years old'
+  return f'Hi. My name is {name} and I\'m {str(age)} years old'
 
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
