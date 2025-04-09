d3ca494142785f6807f2222a2f90583768b420df
ArtemG000 <artemguguev13@gmail.com>
2025-04-09 14:39:26 +0200

    gold added random and usr input

--- Practice/gold.py
+++ Practice/gold.py
@@ -1,20 +1,22 @@
-lvl_1 = [7]
-dungeon = {
-    "lvl_2": [5, 8],
-    "lvl_3": [9, 8, 2],
-    "lvl_4": [1, 3, 5, 6],
-    "lvl_5": [6, 2, 4, 4, 5],
-    "lvl_6": [9, 5, 3, 5, 5, 7],
-    "lvl_7": [7, 4, 6, 4, 7, 6, 8]
-}
-res = lvl_1
+import random
+
+
+lvl_quan = int(input('Enter quantity of levels in this dungeon = '))
+dungeon = {}
+lvl_act = 0
+while lvl_act < lvl_quan:
+    gold_quan = [random.randint(0,9) for i in range(lvl_act + 1)]
+    dungeon[f'lvl_{lvl_act + 1}'] = gold_quan
+    lvl_act += 1
+for i in dungeon.keys():
+    print(dungeon[i])
+res = dungeon['lvl_1']
 s = 0
-for i in range(2, 8):
-    current = dungeon[f"lvl_{i}"]
+for i in range(1, (len(dungeon))):
+    current = dungeon[f"lvl_{i+1}"]
     if current[s] > current[s + 1]:
         res.append(current[s])
     else:
         res.append(current[s + 1])
         s += 1
-
-print(sum(res))
+print(res, 'summa -' , sum(res))
