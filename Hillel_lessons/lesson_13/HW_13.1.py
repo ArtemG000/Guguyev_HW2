dbedc08ed1352a3c3febeab3aeb46f986c9b38a1
ArtemG000 <artemguguev13@gmail.com>
2025-05-02 22:29:00 +0200

    HW_13_1 done

--- Hillel_lessons/lesson_13/HW_13.1.py
+++ Hillel_lessons/lesson_13/HW_13.1.py
@@ -8,7 +8,6 @@
 #Artem Guguyev
 
 class Human:
-
     def __init__(self, gender, age, first_name, last_name):
         self.gender = gender
         self.age = age
@@ -34,10 +33,7 @@ class Group:
         self.group = set()
 
     def add_student(self, student):
-        if len(self.group) < 10:
-            self.group.add(student)
-        else:
-            raise ValueError('The Group is full')
+        self.group.add(student)
 
     def delete_student(self, last_name):
         if self.find_student(last_name) in self.group:
@@ -56,31 +52,15 @@ class Group:
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
-
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
-
+print('1:', gr)
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
 assert gr.find_student('Jobs2') is None, 'Test2'
 assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
 
+gr.delete_student('Taylor')
+print('2:', gr)  # Only one student
 
-st3 = Student('Male', 30, 'Steve', 'Job', 'AN142')
-st4 = Student('Female', 25, 'Liza', 'Taylok', 'AN145')
-st5 = Student('Male', 30, 'Steve', 'Jobyu', 'AN142')
-st6 = Student('Female', 25, 'Liza', 'Taybor', 'AN145')
-st7 = Student('Male', 30, 'Steve', 'Kobs', 'AN142')
-st8 = Student('Female', 25, 'Liza', 'Tiolor', 'AN145')
-st9 = Student('Male', 30, 'Steve', 'Jonnmies', 'AN142')
-st10 = Student('Female', 25, 'Liza', 'Tor', 'AN145')
-st11 = Student('Male', 31, 'Stye', 'Blok', 'AN146')
-
-for i in (st3, st4, st5, st6, st7, st8, st9, st10):
-    gr.add_student(i)
-print(str(gr))
-
-try:
-    gr.add_student(st11)
-except ValueError as error:
-    print(error)
+gr.delete_student('Taylor')  # No error!
