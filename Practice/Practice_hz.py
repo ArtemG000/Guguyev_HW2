c814a538863d7a8ce205ee7f7a9a5c0b695d29e0
ArtemG000 <artemguguev13@gmail.com>
2025-05-25 10:47:54 +0200

    yaml and gold

--- Practice/Practice_hz.py
+++ Practice/Practice_hz.py
@@ -1,82 +1,190 @@
-# # for i in  range(5 ,16 ,2 ):
-# #     print("El: ", i)
-# #
-# # print("\n\n\n\n\n")
-# # some_text = "Some Text"
-# # for i in some_text:
-# #     # if i != "e":
-# #         # print(i)
-#
-#
-# # i = 2
-# # while i < 256:
-# #     i *= 2
-# #     print(i)
-#
-# work = True
-# while work:
-#     user_input = input("Vvedit` 'STOP': ")
-#     if user_input.upper() == 'STOP':
-#         work = False
-#     else:
-#         print("we are ready!")
-# print(' While loop is done!')
+import copy
 
-#
-# for i in "Hello World!":
-#     if i.lower() == 'h':
-#        print("Found")
-#        break
-# else:
-#     print('Not found')
-#
-#
-# x = 2
-# y = 4 / 2
-# print(id(x))
-# print(id(y))
-# if x == y:
-#     print(1)
-# if x is y:
-#     print(2)
-# else:
-#     print('lol')
-
-orange_price = 15
-tea_price = 10
-apple_price = 5
-print('orange price =', orange_price)
-print('tea price =', tea_price)
-print('apple price =', apple_price)
-my_money = float(input('Write how much money You have ='))
-print(my_money)
-if my_money >= orange_price:
-    my_money -= orange_price
-    print('I buy oranges and i have rest money: ', my_money)
-    if my_money >= tea_price:
-        my_money -= tea_price
-        print('I buy tea and i have rest money: ', my_money)
-        if my_money >= apple_price:
-            my_money -= apple_price
-            print('I buy apples and i have rest money: ', my_money)
-            if my_money == 0:
-                print('Oh God.... I have no money')
-            else:
-                print('I have more money - ', my_money)
+def symbols_delete(value: (str, dict, list, None)) -> str:
+    """
+    func return string without '-' and with element after '\\'. Or iterable object
+    """
+    if type(value) is not str:
+        return value
+    if value.startswith('-'):
+        value = value[1:].lstrip()
+        if not value:
+            return None
+    value_lst = []
+    i = 0
+    while i < len(value):
+        if value[i] == '\\' and i + 1 < len(value):
+            value_lst.append(value[i + 1])
+            i += 2
         else:
-            print('I don`t have enough money for apples, but i have bought oranges and tea and have more money- ', my_money)
-    elif my_money >= apple_price:
-        my_money -= apple_price
-        print('I have bought oranges, but i don`t have enough money for tea, so i decided to buy apples and have more money - ', my_money)
-    else:
-        print('I have bought only oranges, but i have money for beer- ', my_money)
-elif my_money >= tea_price:
-    my_money -= tea_price
-    print('I have bought tea and have so much money- ', my_money)
-elif my_money >= apple_price:
-    my_money -= apple_price
-    print('I have bought apples and have so much money- ', my_money)
-else:
-    print('i don`t have enough money for this purchase, because i have only -', my_money)
-print('End of Purchase!')
+            value_lst.append(value[i])
+            i += 1
+    return ''.join(value_lst).strip()
 
+def value_converter(iterable_obj, element_value: str, element_index: (int, str)) -> (bool, int, float, str):
+    """
+    checks and coverts the element from iterable_object
+    element_index either index for list or key for dict
+    """
+
+    bool_dct = {'true': True, 'false': False}
+    if element_value is None or len(element_value) == 0:
+        iterable_obj[element_index] = None
+    elif type(element_value) is not str:
+        iterable_obj[element_index] = element_value
+    elif "null" in element_value.lower() and not (element_value.startswith('"') and element_value.endswith('"')):
+        iterable_obj[element_index] = None
+    elif element_value.isdigit():
+        iterable_obj[element_index] = int(element_value)
+    elif element_value.lower() in bool_dct:
+        iterable_obj[element_index] = bool_dct[element_value.lower()]
+    elif element_value.startswith('"') and element_value.endswith('"'):
+        iterable_obj[element_index] = element_value[1: -1]
+    else:
+        iterable_obj[element_index] = element_value
+
+def deep_add(list_to_save_tmp_deep, list_with_deep_iterable_obj) :
+    """
+    checks the inserted element and creates a list in the correct format
+    """
+    x = copy.deepcopy(list_to_save_tmp_deep)
+    x = yaml('\n'.join(x))
+    list_to_save_tmp_deep.clear()
+    list_with_deep_iterable_obj.append(x)
+
+def yaml(a: (str, list, dict)) -> (dict, list):
+    end_dict = {}
+    end_list = []
+    lst = [i for i in a.splitlines() if i]
+    if any([i.startswith('  ') for i in lst]):
+        temporary_list_with_deep = []
+        list_with_deep_iterable_obj = []
+        for ind, el in enumerate(lst[:-1]):
+            if el.startswith('  ') and lst[ind + 1].startswith('  ') and lst[ind + 1] is lst[-1]:
+                temporary_list_with_deep.append(el[2:])
+                temporary_list_with_deep.append(lst[ind + 1][2:])
+                deep_add(temporary_list_with_deep, list_with_deep_iterable_obj)
+            elif el.startswith('  ') and lst[ind + 1].startswith('  '):
+                temporary_list_with_deep.append(el[2:])
+            elif el.startswith('  ') and not lst[ind + 1].startswith('  '):
+                temporary_list_with_deep.append(el[2:])
+                deep_add(temporary_list_with_deep, list_with_deep_iterable_obj)
+            else:
+                list_with_deep_iterable_obj.append(el)
+            if lst[ind + 1] is lst[-1] and not lst[ind + 1].startswith('  '):
+                if temporary_list_with_deep:
+                    temporary_list_with_deep.append(el[2:])
+                    deep_add(temporary_list_with_deep, list_with_deep_iterable_obj)
+                list_with_deep_iterable_obj.append(lst[ind + 1])
+        lst = list_with_deep_iterable_obj
+    # list
+    if all([":" not in line for line in lst]):
+        for line in lst:
+            if type(line) is not str:
+                end_list.append(line)
+                continue
+            if '"' in line:
+                text_in_line = symbols_delete(line)
+                if text_in_line.startswith('"') and text_in_line.endswith('"'):
+                    end_list.append(text_in_line)
+                    continue
+            # checks the comments
+            if '#' in line:
+                text_in_line = symbols_delete(line.split('#')[0])
+                if text_in_line:
+                    end_list.append(f'{text_in_line}')
+            else:
+                text_in_line = symbols_delete(line)
+                if text_in_line:
+                    end_list.append(f'{text_in_line}')
+                else:
+                    end_list.append(None)
+
+        for i, el in enumerate(end_list):
+            value_converter(end_list, el, i)
+        if any([type(_) in (list, dict) for _ in end_list]):
+            result_list = []
+            for i, el in enumerate(end_list):
+                if el is None and isinstance(end_list[i + 1], (list, dict)) and i != len(end_list):
+                    continue
+                result_list.append(el)
+            end_list = copy.deepcopy(result_list)
+        return end_list
+
+    # dict
+    for ind, key_value in enumerate(lst):
+        if not key_value or isinstance(key_value, (list, dict, int)):
+            continue
+        if key_value.endswith(':') and not key_value is lst[-1] and isinstance(lst[ind + 1], (list, dict)):
+            key = key_value[:-1]
+            value = lst[ind + 1]
+        else:
+            key, value = map(str.strip, key_value.split(':', 1))
+        value = symbols_delete(value)
+        value_converter(end_dict, value, key)
+    print(f'end_dict:{end_dict}')
+    return end_dict
+
+assert yaml("- Alex\n-\n  - odessa\n  - dnipro\n- Li") == [
+    "Alex",
+    ["odessa", "dnipro"],
+    "Li",
+]
+assert yaml("- 67\n-\n  name: Irv\n  game: Mario\n-\n- 56") == [
+    67,
+    {"game": "Mario", "name": "Irv"},
+    None,
+    56,
+]
+assert yaml("name: Alex\nstudy:\n  type: school\n  number: 78\nage: 14") == {
+    "age": 14,
+    "study": {"type": "school", "number": 78},
+    "name": "Alex",
+}
+assert yaml('name: Alex\nstudy:\n  - 89\n  - 89\n  - "Hell"\nage: 14') == {
+    "age": 14,
+    "study": [89, 89, "Hell"],
+    "name": "Alex",
+}
+assert yaml(
+    "name: Alex\nstudy:\n  -\n    type: school\n    num: 89\n  -\n    type: school\n    num: 12\nage: 14"
+) == {
+    "age": 14,
+    "study": [{"num": 89, "type": "school"}, {"num": 12, "type": "school"}],
+    "name": "Alex",
+}
+assert yaml(
+    "name: Alex\nstudy:\n  -\n    type: school\n    nums:\n      - 12\n      - 67\n  -\n    type: school\n    num: 12\nage: 14"
+) == {
+    "age": 14,
+    "study": [{"type": "school", "nums": [12, 67]}, {"num": 12, "type": "school"}],
+    "name": "Alex",
+}
+
+assert yaml('name: "Bob Dylan"\nchildren: 6\ncoding:') == {'coding': None, 'name': 'Bob Dylan', 'children': 6}
+assert yaml("name: Alex Fox\nage: 12\n\nclass: 12b") == {
+    "age": 12,
+    "name": "Alex Fox",
+    "class": "12b",
+}
+assert yaml('name: "Alex Fox"\nage: 12\n\nclass: 12b') == {
+    "name": "Alex Fox",
+    "age": 12,
+    "class": "12b",
+}
+assert yaml('name: "Alex \\"Fox\\""\nage: 12\n\nclass: 12b') == {
+    "name": 'Alex "Fox"',
+    "age": 12,
+    "class": "12b",
+}
+assert yaml('- true\n- false\n- "false"\n- "null"\n- null') == [True, False, "false", "null", None]
+
+assert yaml('- write some\n- "Alex Chii"\n- 89') == ["write some", "Alex Chii", 89]
+assert yaml("- 1\n- 2\n- 3\n\n- 4\n\n\n\n- 5") == [1, 2, 3, 4, 5]
+assert yaml("-\n-\n-\n- 7") == [None, None, None, 7]
+assert yaml(
+    '# comment\n- write some # after\n# one mor\n- "Alex Chii #sir "\n- 89 #bl'
+) == ["write some", "Alex Chii #sir ", 89]
+assert yaml('key: 1\nactive: true\nname: "Tester"\nflag: false\nnone: null') == {
+    'key': 1, 'active': True, 'name': 'Tester', 'flag': False, 'none': None
+}
--- Practice/gold.py
+++ Practice/gold.py
@@ -1,22 +1,73 @@
-import random
+def count_gold(pyramid: list[list[int]]) -> int:
+    # replace this for solution
+    pyramid_reversed = pyramid[::-1]
+    for ind, level in enumerate(pyramid_reversed[:-1]):
+        for i, path in enumerate(level[:-1]):
+            pyramid_reversed[ind + 1][i] += max(path, level[i + 1])
+    print(pyramid_reversed)
+    return int(*pyramid_reversed[-1])
 
+print(
+    count_gold(
+        [
+            [1],
+            [2, 3],
+            [3, 3, 1],
+            [3, 1, 5, 4],
+            [3, 1, 3, 1, 3],
+            [2, 2, 2, 2, 2, 2],
+            [5, 6, 4, 5, 6, 4, 3],
+        ]
+    )
+)
 
-lvl_quan = int(input('Enter quantity of levels in this dungeon = '))
-dungeon = {}
-lvl_act = 0
-while lvl_act < lvl_quan:
-    gold_quan = [random.randint(0,9) for i in range(lvl_act + 1)]
-    dungeon[f'lvl_{lvl_act + 1}'] = gold_quan
-    lvl_act += 1
-for i in dungeon.keys():
-    print(dungeon[i])
-res = dungeon['lvl_1']
-s = 0
-for i in range(1, (len(dungeon))):
-    current = dungeon[f"lvl_{i+1}"]
-    if current[s] > current[s + 1]:
-        res.append(current[s])
-    else:
-        res.append(current[s + 1])
-        s += 1
-print(res, 'summa -' , sum(res))
+# These "asserts" are used for self-checking
+assert (
+        count_gold(
+            [
+                [1],
+                [2, 3],
+                [3, 3, 1],
+                [3, 1, 5, 4],
+                [3, 1, 3, 1, 3],
+                [2, 2, 2, 2, 2, 2],
+                [5, 6, 4, 5, 6, 4, 3],
+            ]
+        )
+        == 23
+)
+assert (
+        count_gold(
+            [
+                [1],
+                [2, 1],
+                [1, 2, 1],
+                [1, 2, 1, 1],
+                [1, 2, 1, 1, 1],
+                [1, 2, 1, 1, 1, 1],
+                [1, 2, 1, 1, 1, 1, 9],
+            ]
+        )
+        == 15
+)
+assert count_gold([[9], [2, 2], [3, 3, 3], [4, 4, 4, 4]]) == 18
+assert (
+        count_gold(
+            [[2], [7, 9], [0, 8, 6], [4, 7, 6, 8], [0, 5, 5, 4, 1], [9, 1, 0, 1, 6, 9]]
+        )
+        == 35
+)
+assert (
+        count_gold(
+            [
+                [4],
+                [3, 0],
+                [5, 1, 1],
+                [2, 0, 2, 0],
+                [1, 1, 1, 8, 3],
+                [5, 3, 4, 8, 2, 8],
+                [1, 0, 5, 0, 4, 3, 1],
+            ]
+        )
+        == 30
+)
